4035ede80a9ad7664db41a14c1d77b274ef4e342
Shiladitya Basu <shiladitya.31.z@gmail.com>
2020-02-02 14:02:44 +0530

    external link

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, shell } from 'electron'
 import {
   createProtocol
   //installVueDevtools
@@ -23,13 +23,17 @@ function createWindow () {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    // if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
 
+  win.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
   win.on('closed', () => {
     win = null
   })
--- src/components/tile.vue
+++ src/components/tile.vue
@@ -7,9 +7,10 @@
         <div class="content-overlay"></div>
         <div class="content">
           <p>U/{{ post.data.author }}</p>
+          <a target="_blank" v-bind:href=" 'http://www.reddit.com' +post.data.permalink"><i class="fas fa-globe fa-lg" style="color: white;"></i></a>
         </div>
       </div>
-      
+      <hr class="my-2 style2">
     </li>
   </ul>
 </div>
@@ -21,7 +22,6 @@
 <script>
   import 'bootstrap'
   import 'bootstrap/dist/css/bootstrap.min.css'
-
 
   const axios = require('axios')
   //const jquery = require('jquery')
@@ -42,10 +42,8 @@
     created() {
       axios.get(baseURL)
         .then(response => {
-
           this.postObjList = response.data.data.children
           console.log(this.postObjList)
-
         })
 
     }
@@ -54,115 +52,131 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+@import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+* {
+  font-family: 'Quicksand';
+}
 
-  li {
-    display: inline-block;
-    padding: 5px;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
+li {
+  display: inline-block;
+  padding: 5px;
+}
 
-  img {
-    //padding: .2em;
-    height: auto;
-    width: 100%;
-    border-radius: .5em;
-    //display: block;
-  }
+hr.style2{
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0));
+}
 
-  //for the hover effect
-  //https://codepen.io/ArnaudBalland/pen/vGZKLr?editors=1100
+img {
+  //padding: .2em;
+  height: auto;
+  width: 100%;
+  //border-radius: .5em;
+  //display: block;
+}
 
-  .gallery {
-  column-count: 3;
-  column-width: 33%;
-  }
+//for the hover effect
+//https://codepen.io/ArnaudBalland/pen/vGZKLr?editors=1100
 
-  @media (max-width: 700px) {
-  .gallery {
-    -webkit-column-count: 2;
-    -moz-column-count: 2;
-    column-count: 2;
-    column-width: 50%;}
-  }
+.gallery {
+column-count: 3;
+column-width: 33%;
+}
 
-  @media (max-width: 450px) {
-  .gallery {
-    -webkit-column-count: 1;
-    -moz-column-count: 1;
-    column-count: 1;
-    column-width: 100%;}
-  }
+@media (max-width: 700px) {
+.gallery {
+  -webkit-column-count: 2;
+  -moz-column-count: 2;
+  column-count: 2;
+  column-width: 50%;}
+}
 
-  .card {
-    border-radius: .5em;
-  }
+@media (max-width: 450px) {
+.gallery {
+  -webkit-column-count: 1;
+  -moz-column-count: 1;
+  column-count: 1;
+  column-width: 100%;}
+}
 
-  .pics .card:hover {
-    // margin: 2px;
-    // padding: 2px;
-    //transform: scale(1.05);
-    cursor: zoom-in;
-    box-shadow: 
-      -7px 7px 12px rgba(255,255,255,.3),
-      7px 7px 12px rgba(0, 0, 0, .3);
-    
-  }
+.card {
+  //border-radius: .5em;
+}
 
-  .card .content-overlay {
-    border-radius: .5em;
-    background: rgba(0,0,0,0.5);
-    position: absolute;
-    align-content: right;
-    height: 100%;
-    width: 100%;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    opacity: 0;
-    -webkit-transition: all 0.4s ease-in-out 0s;
-    -moz-transition: all 0.4s ease-in-out 0s;
-    transition: all 0.4s ease-in-out 0s;
-  }
+hr.style2 {
+  //display: none;
+  opacity: 0;
+}
 
-  .card:hover img {
-    filter: blur(2px);
-  }
-
-  .card:hover .content-overlay{
-    opacity: 1;
-  }
-
-  .card .content {
-    font-size: x-small;
-    color: white;
-    letter-spacing: 0.25rem;
-    position: absolute;
-    text-align: center;
-    padding-left: 1em;
-    padding-right: 1em;
-    width: 100%;
-    top: 80%;
-    left: 50%;
-    opacity: 0;
-    -webkit-transform: translate(-50%, -50%);
-    -moz-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    -webkit-transition: all 0.3s ease-in-out 0s;
-    -moz-transition: all 0.3s ease-in-out 0s;
-    transition: all 0.3s ease-in-out 0s;
-  }
-
-
-  .card:hover .content {
-    top: 50%;
-    left: 50%;
-    opacity: 1;
-  }
-
+.pics .card:hover {
+  // margin: 2px;
+  // padding: 2px;
+  //transform: scale(1.05);
+  cursor: zoom-in;
+  box-shadow: 
+    -7px 7px 12px rgba(255,255,255,.3),
+    7px 7px 12px rgba(0, 0, 0, .3);
   
+}
+
+.card .content-overlay {
+  //border-radius: .4em;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.6));
+  position: absolute;
+  align-content: right;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0;
+  -webkit-transition: all 0.4s ease-in-out 0s;
+  -moz-transition: all 0.4s ease-in-out 0s;
+  transition: all 0.4s ease-in-out 0s;
+}
+
+.card:hover img {
+  filter: blur(2px);
+}
+
+.card:hover .content-overlay{
+  opacity: 1;
+}
+
+
+
+.card .content {
+  font-size: x-small;
+  color: white;
+  letter-spacing: 0.25rem;
+  position: absolute;
+  text-align: center;
+  padding-left: 1em;
+  padding-right: 1em;
+  width: 100%;
+  top: 80%;
+  left: 50%;
+  opacity: 0;
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  -webkit-transition: all 0.3s ease-in-out 0s;
+  -moz-transition: all 0.3s ease-in-out 0s;
+  transition: all 0.3s ease-in-out 0s;
+}
+
+
+.card:hover .content {
+  top: 50%;
+  left: 50%;
+  opacity: 1;
+}
+
 </style>
